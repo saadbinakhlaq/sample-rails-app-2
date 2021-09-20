831d741c1a33b8a7ecffaa0cf375abf421f9c49d
Saad Bin Akhlaq <saad.18@gmail.com>
2021-09-20 14:59:45 +0200

    Add tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,7 +5,7 @@ class Category < ApplicationRecord
   has_many :subcategories, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
   belongs_to :parent_category, class_name: 'Category', optional: true
 
-  validates :name, uniqueness: { case_sensitive: false }
+  validates :name, uniqueness: { case_sensitive: false }, presence: true
   validate :parent_category_id_is_not_self, on: :update
 
   def parent_category_id_is_not_self
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   describe 'validations' do
+    subject { build(:category) }
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+
     describe '#parent_category_id_is_not_self' do
       it 'does not update parent_category_id with self' do
         category = FactoryBot.create(:category)
