1fe63b3ade4c311e5be3d578a32f7d3b2e109723
Saad Bin Akhlaq <saad.18@gmail.com>
2021-09-25 17:35:58 +0200

    Add account

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_25_141112) do
+ActiveRecord::Schema.define(version: 2021_09_25_153046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "name", default: ""
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -124,6 +132,7 @@ ActiveRecord::Schema.define(version: 2021_09_25_141112) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "categories", "categories", column: "parent_category_id"
   add_foreign_key "categorizations", "businesses"
   add_foreign_key "categorizations", "categories"
