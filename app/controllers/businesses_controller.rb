c1295cf3e6eaec7e6442c29f70b545512a74c8c2
Saad Bin Akhlaq <saad.18@gmail.com>
2021-09-26 16:18:31 +0200

    Add reviews

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -5,6 +5,13 @@ class BusinessesController < ApplicationController
 
   def show
     business = Business.find(params[:id])
+    all_reviews = business.reviews.published
+    @reviews = all_reviews.page(params[:page]).per(5)
+    @five_rating_count = all_reviews.five_rating_count
+    @four_rating_count = all_reviews.four_rating_count
+    @three_rating_count = all_reviews.three_rating_count
+    @two_rating_count = all_reviews.two_rating_count
+    @one_rating_count = all_reviews.one_rating_count
     @business = BusinessPresenter.new(business)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def calculate_percentage(value_now, total_value)
+    puts "inital valye #{value_now}"
+    puts "final valye #{total_value}"
+    value = (value_now.to_f/total_value) * 100
+
+    puts value
+    value
+  end
 end
